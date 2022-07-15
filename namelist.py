b2ee84d9a54d9788a7684999141a1868968a07ce
ariduki <ariduki@gmail.com>
2022-07-15 23:21:43 +0800

    add namelist.py

---
+++ namelist.py
@@ -0,0 +1,26 @@
+
+namelist = set()
+alias_map = {}
+
+
+def load_namelist_from_file():
+    with open('./blacklist/blacklist.txt', 'r', encoding='utf-8') as f:
+        lines = f.read().splitlines()
+        for i in range(len(lines)//2):
+            id = lines[i*2]
+            alias = lines[i*2+1]
+            add(id, alias)
+
+
+def add(id, alias):
+    namelist.add(id)
+    alias_map[id] = alias
+
+
+def get_list():
+    load_namelist_from_file()
+    return namelist
+
+
+def get_name_map():
+    return alias_map
