9fcd550bc4e054c95581304b392fd9814d0caf9d
ariduki <ariduki@gmail.com>
2022-07-16 00:39:45 +0800

    implement check_teams

--- observer.py
+++ observer.py
@@ -13,7 +13,19 @@ def check_players():
 
 
 def check_teams():
-    pass
+    teams = live_game.get_in_game_teams()
+    res = []
+    for players in teams:
+        valid_id = []
+        for steam_id, race_id in players:
+            if steam_id in id_list:
+                print(steam_id,mp[steam_id])
+                valid_id.append(mp[steam_id])
+        if valid_id:
+            team_name = ' & '.join(valid_id)
+            ppl = len(players)
+            res.append([team_name, ppl])
+    return res
 
 
 id_list = namelist.get_list()
