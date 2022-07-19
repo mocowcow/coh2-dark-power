19384c4685c189e26bea7348d66a81ab29edea6a
ariduki <ariduki@gmail.com>
2022-07-19 11:33:09 +0800

    fix bug

--- live_game.py
+++ live_game.py
@@ -39,11 +39,13 @@ def get_in_game_players():
     return res
 
 
-def get_live_games():
-    url = 'https://us-east4-coh2-ladders-prod.cloudfunctions.net/getLiveGamesHttp?playerGroup=4&start=0&count=50&sortOrder=0&apiKey=kmLw58rYG2CWBLQr'
+def get_live_games(start=0, count=100):
+    url = f'https://us-east4-coh2-ladders-prod.cloudfunctions.net/getLiveGamesHttp?playerGroup=4&start={start}&count={count}&sortOrder=0&apiKey=kmLw58rYG2CWBLQr'
     res = requests.get(url)
     if res.status_code != 200:
         raise Exception('failed to fetch data')
     res.encoding = ('utf-8')
     games = json.loads(res.text)
+    if len(games) == count:
+        games += get_live_games(start=start+count, count=count)
     return games
